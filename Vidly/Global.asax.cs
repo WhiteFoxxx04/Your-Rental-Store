462c3bfbe45d31e5b1fbd9b76f5653ea98e786b7
Asghar Khan <asgarkhan0405@outlook.com>
2020-11-09 01:25:39 +0530

    Created an API for Customers

--- Vidly/Global.asax.cs
+++ Vidly/Global.asax.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using AutoMapper;
+using Vidly.App_Start;
 using Vidly.Models;
 
 namespace Vidly
@@ -14,6 +17,8 @@ namespace Vidly
     {
         protected void Application_Start()
         {
+            Mapper.Initialize(c => c.AddProfile<MappingProfile>());
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             Database.SetInitializer<ApplicationDbContext>(null);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
