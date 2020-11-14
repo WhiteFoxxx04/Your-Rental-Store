5da12639801dc6126d255178777cd040c47a122f
Asghar Khan <asgarkhan0405@outlook.com>
2020-11-15 03:16:33 +0530

    removed those migration for some reason

--- Vidly/Migrations/Configuration.cs
+++ Vidly/Migrations/Configuration.cs
@@ -9,7 +9,7 @@ namespace Vidly.Migrations
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(Vidly.Models.ApplicationDbContext context)
